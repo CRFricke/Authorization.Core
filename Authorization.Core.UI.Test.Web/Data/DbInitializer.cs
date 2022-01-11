1c30676114080473550472cddf70fc3fdf037c00
Chuck Fricke <80228320+CRFricke@users.noreply.github.com>
2022-01-11 18:51:13 -0500

    Make DbInitializer class synchronous.

--- Authorization.Core.UI.Test.Web/Data/DbInitializer.cs
+++ Authorization.Core.UI.Test.Web/Data/DbInitializer.cs
@@ -2,24 +2,23 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Threading.Tasks;
 
 namespace Authorization.Core.UI.Test.Web.Data
 {
     public static class DbInitializer
     {
-        public static async Task SeedDatabaseAsync(IServiceProvider services)
+        public static void InitializeDatabase(IServiceProvider services)
         {
             var logger = services.GetRequiredService<ILoggerFactory>().CreateLogger(typeof(DbInitializer));
             var dbContext = services.GetRequiredService<ApplicationDbContext>();
 
             try
             {
-                await dbContext.Database.MigrateAsync();
+                dbContext.Database.Migrate();
             }
             catch (Exception ex)
             {
-                logger.LogError(ex, "Database.MigrateAsync() failed.");
+                logger.LogError(ex, "Database.Migrate() failed.");
                 throw;
             }
 
