e1b10b9d9caf1e18e0a8dda0d6885f20555fe36b
Chuck Fricke <80228320+CRFricke@users.noreply.github.com>
2024-05-26 22:50:12 -0400

    Force login on Admin Index page.

--- Authorization.Core.UI.Test.Web/Areas/Admin/Pages/Index.cshtml.cs
+++ Authorization.Core.UI.Test.Web/Areas/Admin/Pages/Index.cshtml.cs
@@ -1,8 +1,9 @@
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Authorization.Core.UI.Test.Web.Areas.Admin.Pages
 {
+    [Authorize()]
     public class IndexModel : PageModel
     {
         public void OnGet()
